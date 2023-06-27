544427793ccf1d35ff41843e15fad04e1360b031
Hyeongchan Shin <intshc@gmail.com>
2023-06-27 18:53:14 +0900

    표 꾸미기(with mui 컴포넌트)

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,8 +1,17 @@
 import React, {useEffect, useState} from "react";
 import {SERVER_URL} from "../constants";
+import {DataGrid} from "@mui/x-data-grid";
 
 function Carlist(){
   const [cars, setCars] = useState([]);
+
+  const columns =[
+    {field: 'brand', headerName: 'Brand', width: 200},
+    {field: 'model', headerName: 'Model', width: 200},
+    {field: 'color', headerName: 'Color', width: 200},
+    {field: 'year', headerName: 'Year', width: 200},
+    {field: 'price', headerName: 'Price', width: 200},
+  ];
 
   useEffect(()=>{
     fetch(SERVER_URL + 'api/cars')
@@ -13,20 +22,10 @@ function Carlist(){
 
   return(
           <div>
-            <table>
-              <tbody>
-              {
-                cars.map((car, index) =>
-                <tr key={index}>
-                  <td>{car.brand}</td>
-                  <td>{car.model}</td>
-                  <td>{car.color}</td>
-                  <td>{car.year}</td>
-                  <td>{car.price}</td>
-                </tr>)
-              }
-              </tbody>
-            </table>
+            <DataGrid
+            rows={cars}
+            columns={columns}
+            getRowId={row => row._links.self.href}/>
           </div>
   )
 }
