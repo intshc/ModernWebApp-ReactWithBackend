72bda4788a24db9ff956392a56eb97c8869d0fe0
Hyeongchan Shin <intshc@gmail.com>
2023-06-28 23:54:08 +0900

    데이터 삭제기능 추가

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -2,31 +2,49 @@ import React, {useEffect, useState} from "react";
 import {SERVER_URL} from "../constants";
 import {DataGrid} from "@mui/x-data-grid";
 
-function Carlist(){
+function Carlist() {
   const [cars, setCars] = useState([]);
 
-  const columns =[
+  const onDelClick = (url) => {
+    fetch(url, {method: 'DELETE'})
+            .then(response => fetchCars())
+  }
+  const columns = [
     {field: 'brand', headerName: 'Brand', width: 200},
     {field: 'model', headerName: 'Model', width: 200},
     {field: 'color', headerName: 'Color', width: 200},
     {field: 'year', headerName: 'Year', width: 200},
     {field: 'price', headerName: 'Price', width: 200},
+    {
+      field: '_links.self.href',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      renderCell: row =>
+              <button
+                      onClick={(() => onDelClick(row.id))}>Delete
+              </button>
+    }
   ];
+  useEffect(() => {
+    fetchCars();
+  }, []);
 
-  useEffect(()=>{
+  const fetchCars = () => {
     fetch(SERVER_URL + 'api/cars')
             .then(response => response.json())
             .then(data => setCars(data._embedded.cars))
             .catch(err => console.error(err));
-  },[]);
+  }
 
-  return(
+  return (
           <div>
             <DataGrid
-            rows={cars}
-            columns={columns}
-            getRowId={row => row._links.self.href}/>
+                    rows={cars}
+                    columns={columns}
+                    getRowId={row => row._links.self.href}/>
           </div>
   )
 }
+
 export default Carlist;
