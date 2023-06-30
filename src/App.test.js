c4640474b997a54be1544ed2360f7bc7aa84a330
Hyeongchan Shin <intshc@gmail.com>
2023-06-30 17:52:03 +0900

    Jest를 사용한 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import AddCar from "./components/AddCar";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a snapshot', () => {
+  render(<App/>);
+  const tree = TestRenderer.create(<AddCar/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+test('open add car modal from', async () => {
+  render(<App/>);
+  fireEvent.click(screen.getByText('New Car'));
+  expect(screen.getByRole('dialog')).toHaveTextContent('New car');
+})
